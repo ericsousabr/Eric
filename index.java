19291cf78d7f77582286ca002dee9f9d6f9503ce
ericsousabr <ericsousa0305@gmail.com>
2018-06-05 19:10:22 -0300

    first commit

---
+++ index.java
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Index</title>
+</head>
+<body>
+	<h1><center>Tag H1</center></h1><br>
+	<HEADER><center>Cabeçalho</center></HEADER><br>
+	<section><center>Parte 1</center></section><br>
+	<nav><center>www.google.com.br</center></nav><br>
+	<footer>Rodapé do site</footer>
+</body>
+</html>
